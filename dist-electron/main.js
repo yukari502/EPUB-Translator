a1f44380e097aab1d34716e818cdbb8080b544ef
Mona <zhixi221@gmail.com>
2026-05-05 19:39:48 +0800

    fix

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,34 +1,25 @@
-import { BrowserWindow, app } from "electron";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { BrowserWindow as e, app as t } from "electron";
+import n from "node:path";
+import { fileURLToPath as r } from "node:url";
 //#region electron/main.ts
-var __dirname = path.dirname(fileURLToPath(import.meta.url));
-process.env.APP_ROOT = path.join(__dirname, "..");
-var VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
-var MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-var RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
-var win;
-function createWindow() {
-	win = new BrowserWindow({
+var i = n.dirname(r(import.meta.url));
+process.env.APP_ROOT = n.join(i, "..");
+var a = process.env.VITE_DEV_SERVER_URL, o = n.join(process.env.APP_ROOT, "dist-electron"), s = n.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = a ? n.join(process.env.APP_ROOT, "public") : s;
+var c;
+function l() {
+	c = new e({
 		width: 1200,
 		height: 800,
-		icon: path.join(process.env.VITE_PUBLIC, "favicon.svg"),
+		icon: n.join(process.env.VITE_PUBLIC, "favicon.svg"),
 		webPreferences: {
-			preload: path.join(__dirname, "preload.mjs"),
-			webSecurity: false
+			preload: n.join(i, "preload.mjs"),
+			webSecurity: !1
 		}
-	});
-	win.setMenu(null);
-	if (VITE_DEV_SERVER_URL) win.loadURL(VITE_DEV_SERVER_URL);
-	else win.loadFile(path.join(RENDERER_DIST, "index.html"));
+	}), c.setMenu(null), a ? c.loadURL(a) : c.loadFile(n.join(s, "index.html"));
 }
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-		win = null;
-	}
-});
-app.whenReady().then(createWindow);
+t.on("window-all-closed", () => {
+	process.platform !== "darwin" && (t.quit(), c = null);
+}), t.whenReady().then(l);
 //#endregion
-export { MAIN_DIST, RENDERER_DIST, VITE_DEV_SERVER_URL };
+export { o as MAIN_DIST, s as RENDERER_DIST, a as VITE_DEV_SERVER_URL };
